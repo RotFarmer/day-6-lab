0c520204c6b96ba38f9ef6dddbe0d46ceb6edddd
datyosefguy <datyosefguy@gmail.com>
2020-10-19 17:21:39 -0400

    fixed challenge further

--- js-challenge/script.js
+++ js-challenge/script.js
@@ -1,17 +1,26 @@
-let hours = 45;
-let wage = 20;
-let pay = wage * hours;
-if (hours > 40) {
-    wage * 1.5
+let hours = 50;
+let wage = 10;
+let overtime = (hours - 40)
+let pay = (wage * hours);
+console.log(overtime);
+let otPay = overtime * (wage * 1.5);
+console.log(otPay);
+if (hours >= 40) {
+    hours = 40
+    pay = wage * hours + otPay
     console.log(pay)
 } else {
     console.log(pay)
 };
 let weeks = 1;
 let total = pay;
-while (total <= 1000000) {
-    weeks++;
-    total += pay;
-    console.log(total)
+while (true) {
+    if (total < 1000000) {
+        weeks++;
+        total += pay;
+        console.log(total);
+    } else {
+        break;
+    }
 }
 console.log(`it took ${weeks} weeks to get to ${total} dollars`)
